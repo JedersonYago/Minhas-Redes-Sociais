9aa4af769a860c7b348135310db033499a33ab89
JedersonYago <159579186+JedersonYago@users.noreply.github.com>
2026-08-29 14:15:03 +0000

    Melhorando o design das redes sociais

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("theme-toggle")
+
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+});
